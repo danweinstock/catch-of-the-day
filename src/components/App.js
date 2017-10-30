6547a8a38b82b97a371380690efd059709e8d734
Dan Weinstock <dweinstock@kinjunxion.com>
2017-10-30 15:43:12 -0400

    finished add state logic for fish end of video 13

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
-
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+	constructor(){
+		super(); 
+
+		this.addFish = this.addFish.bind(this);
+		this.state = {
+			fishes: {},
+			order: {}
+		}
+	}
+
+	addFish(fish){
+		// update state
+		// copy of current state
+		const fishes = {...this.state.fishes};
+		// add in our new fish
+		const timestamp = Date.now();
+		fishes[`fish-${timestamp}`] = fish;
+		// set state
+		// this.setState({ fishes: fishes})
+		this.setState({ fishes });
+	}
 	render(){
 		return (
 				<div className="catch-of-the-day">
@@ -12,7 +32,7 @@ class App extends React.Component {
 							<Header tagline="Fresh Seafood Market"/>
 					</div>
 						<Order />
-						<Inventory />
+						<Inventory addFish={this.addFish}/>
 				</div>
 			)
 	}
