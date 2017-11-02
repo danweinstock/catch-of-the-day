5bee9878029f65373a2822fe98580bb2ba847f9b
Dan Weinstock <dweinstock@kinjunxion.com>
2017-11-02 18:10:58 -0400

    sample fishes loading into state not displaying yet

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,16 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 
+import sampleFishes from '../sample-fishes';
+
 class App extends React.Component {
 	constructor(){
 		super(); 
 
 		this.addFish = this.addFish.bind(this);
+		this.loadSamples = this.loadSamples.bind(this);
+
+		// initial state
 		this.state = {
 			fishes: {},
 			order: {}
@@ -25,6 +30,12 @@ class App extends React.Component {
 		// this.setState({ fishes: fishes})
 		this.setState({ fishes });
 	}
+
+	loadSamples(){
+		this.setState({
+			fishes: sampleFishes
+		})
+	}
 	render(){
 		return (
 				<div className="catch-of-the-day">
@@ -32,7 +43,7 @@ class App extends React.Component {
 							<Header tagline="Fresh Seafood Market"/>
 					</div>
 						<Order />
-						<Inventory addFish={this.addFish}/>
+						<Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
 				</div>
 			)
 	}
